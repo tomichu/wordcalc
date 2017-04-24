3a24a2b33f4a9634c7539e84487132cc57d13c6f
Shugen Tomita <shugentomita@gmail.com>
2017-04-24 13:00:22 +0900

    modified index file

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170418034136) do
+ActiveRecord::Schema.define(version: 20170423171056) do
 
   create_table "count_words", force: :cascade do |t|
     t.string   "title"
     t.text     "text_to_count"
     t.datetime "created_at",    null: false
     t.datetime "updated_at",    null: false
+  end
+
+  create_table "datepickers", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "gw_calculations", force: :cascade do |t|
